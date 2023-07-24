8950f77b8274057256474ea609dc9a58f63453e7
Mohammad I-K Sakur <86509345+I-K-Sakur@users.noreply.github.com>
2023-07-24 21:46:08 +0600

    Create Buy a Shovel.cpp

---
+++ Programming Code (Newer)/Buy a Shovel.cpp
@@ -0,0 +1,22 @@
+//https://codeforces.com/contest/732/problem/A
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+int a,b;
+cin>>a>>b;
+int sum=0;
+for(int i=1;;i++)
+{
+    sum=sum+a;
+    if((sum%10==0) || (sum%10==b))
+    {
+        cout<<i<<endl;
+        break;
+    }
+    else{
+        continue;
+    }
+}
+
+}
