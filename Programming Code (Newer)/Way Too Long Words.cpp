9b8d66443cfdfaee568b5f361dbaaa09c882163a
Mohammad I-K Sakur <86509345+I-K-Sakur@users.noreply.github.com>
2023-07-30 01:14:42 +0600

    Create Way Too Long Words.cpp

---
+++ Programming Code (Newer)/Way Too Long Words.cpp
@@ -0,0 +1,22 @@
+//https://codeforces.com/contest/71/problem/A
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+while(n--)
+{
+string s;
+cin>>s;
+if(s.size()>10)
+{
+cout<<s[0]<<s.size()-2<<s[s.size()-1]<<endl;
+}
+else
+{
+    cout<<s<<endl;
+}
+}
+
+}
