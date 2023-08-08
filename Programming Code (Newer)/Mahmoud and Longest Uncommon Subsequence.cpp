28da04884f43e52031ef87d8743ace0243937769
Mohammad I-K Sakur <86509345+I-K-Sakur@users.noreply.github.com>
2023-08-08 20:40:18 +0600

    Create Mahmoud and Longest Uncommon Subsequence.cpp

---
+++ Programming Code (Newer)/Mahmoud and Longest Uncommon Subsequence.cpp
@@ -0,0 +1,19 @@
+//https://codeforces.com/contest/766/problem/A
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  string a,b;
+  int sum=0,size;
+  set<int>s;
+  set<int>::iterator it;
+  it=s.begin();
+ 
+  bool isyes=false;
+  cin>>a>>b;
+if(a==b) cout<<"-1"<<endl;
+else{
+  cout<<max(a.size(),b.size());
+}
+}
+
