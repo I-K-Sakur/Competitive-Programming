0f6cd7191efc80067a1cdaec7f7848f48e9eeb51
Mohammad I-K Sakur <86509345+I-K-Sakur@users.noreply.github.com>
2024-01-24 00:57:34 +0600

    Create codefile.cpp

---
+++ Programming Code (Newer)/codefile.cpp
@@ -0,0 +1,40 @@
+//bitwise all possible subset
+#include <bits/stdc++.h>
+using namespace std;
+
+//given an array string we are searching 
+//for all possiblesubsets
+void possiblesubset(char arr[],int x)
+{
+   //outer loop that iterates upto 2^x
+   //1<<N == 2^x
+   //cause the number of subset is 2^x
+   for(int i=0;i<(1<<x);++i)
+   {
+    //inner loop that iterates over each bit of the current value of i
+     for(int j=0;j<x;j++)
+     {
+        //checking if the j-th bit of i is set 
+        //that means it is 1
+        if(i & (1<<j))
+        {
+           //if set then print the elements
+           cout<<arr[j]<<' ';
+        }
+     }
+     cout<<endl;
+   }
+}
+int main()
+{
+  int n;
+  cin>>n;
+  char arr[100+5];
+  for(int i=0;i<n;i++)
+  {
+    cin>>arr[i];
+  }
+  possiblesubset(arr,n);
+  
+
+ }
