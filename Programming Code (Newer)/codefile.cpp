4ecb2893a87d89be952ec9af864138a3b9daad11
Mohammad I-K Sakur <86509345+I-K-Sakur@users.noreply.github.com>
2024-01-26 00:17:14 +0600

    Update codefile.cpp

--- Programming Code (Newer)/codefile.cpp
+++ Programming Code (Newer)/codefile.cpp
@@ -108,3 +108,37 @@ int main()
 
     return 0;
 }
+
+--------------------------------------------------------------------------------------
+  -------------------------------------------------------------------------------------
+
+  //GCD AND LCM
+
+  #include <bits/stdc++.h>
+using namespace std;
+//gcd and LCM
+int  gcd(int a,int b)
+{
+    int rem=0;
+    while(b!=0)
+    {
+        rem=a%b;
+        a=b;
+        b=rem;
+    }
+    return a;
+}
+
+int lcm(int a,int b)
+{
+   return (a*b)/gcd(a,b);
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    // cout<<__gcd(a,b);
+    cout<<gcd(a,b)<<endl;
+    cout<<lcm(a,b)<<endl;
+}
