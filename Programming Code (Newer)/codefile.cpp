920c644b45a300cfb7368dfc59394666718d9fd8
Mohammad I-K Sakur <86509345+I-K-Sakur@users.noreply.github.com>
2024-02-04 11:41:55 +0600

    Update codefile.cpp

--- Programming Code (Newer)/codefile.cpp
+++ Programming Code (Newer)/codefile.cpp
@@ -643,3 +643,25 @@ int main()
     }
     while(next_permutation(s.begin(),s.end()));
 }
+----------------------------------------------------------------------------
+-----------------------------------------------------------------------------
+/* FACTORIAL USING RECURSION */
+  #include <bits/stdc++.h>
+using namespace std;
+ 
+ int fact(int n)
+ {
+    if(n==0) return 1;
+    else{
+        return n*fact(n-1);
+    }
+ }
+
+int main()
+{
+    int n;
+    cin>>n;
+   cout<< fact(n)<<endl;
+
+   
+}
