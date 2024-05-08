5c525b692acdb013506497d2bc1dffe3c53e2fe2
Mohammad I-K Sakur <86509345+I-K-Sakur@users.noreply.github.com>
2024-05-08 19:30:34 +0600

    Update codefile.cpp

--- Programming Code (Newer)/codefile.cpp
+++ Programming Code (Newer)/codefile.cpp
@@ -849,3 +849,22 @@ int main()
     cin>>n;
    cout<<digitcount(n);
 }
+
+
+//Fibonacci pattern with Recursion
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibonacci(int n)
+{
+  if(n<=1) return n;
+  return fibonacci(n-1)+fibonacci(n-2);
+}
+
+int main()
+{
+  int n;
+  cin>>n;
+  cout<<fibonacci(n)<<endl;
+}
+
